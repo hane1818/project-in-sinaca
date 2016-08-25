e2d7a69e02132ee025bdc1861b35d578dc366052
Hane Liu <hane0131@gmail.com>
2016-08-25 12:08:41 +0800

    Add function to run model & save model per epoch

--- main.py
+++ main.py
@@ -1,34 +1,73 @@
+import time
+
+import numpy as np
+import tensorflow as tf
+
 from dataloader import DataLoader
 from model import Model
 
-import tensorflow as tf
+
+def run_epoch(session, m, data, eval_op, verbose=False):
+    epoch_size = (len(data) // m.batch_size)
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = m.initial_state.eval()
+    for step in range(epoch_size):
+        x, y = data.get_batch(m.batch_size)
+        cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+                                     {m.input: x,
+                                      m.label: y,
+                                      m.initial_state: state})
+        costs += cost
+        iters += 1
+
+        if verbose and step % (epoch_size // 10) == 10:
+            print("%.3f perplexity: %.3f speed: %.0f wps" %
+                  (step * 1.0 / epoch_size, np.exp(costs / iters),
+                   iters * m.batch_size / (time.time() - start_time)))
+
+    return np.exp(costs / iters)
 
 
 def main():
+    batch_size = 100
+    max_epoch = 10
     data = DataLoader()
-    data.separate_data()
-    X, y = data.train.get_batch(10)
-    #input = [[[0] * data.vocab_size for j in range(len(X[i]))] for i in range(len(X))]
-    for i, batch in enumerate(X):
-        for t, value in enumerate(batch):
-            n_hot = [0] * data.vocab_size
-            for v in value:
-                n_hot[v] = 1
-            X[i][t] = n_hot
-
-    """for i, batch in enumerate(y):
-        for t, value in enumerate(batch):
-            one_hot = [0] * data.vocab_size
-            one_hot[value] = 1
-            y[i][t] = one_hot
-
-    print(y)"""
-    #
+    train_data, valid_data, test_data = data.separate_data()
+    # X, y = train.get_batch(batch_size=100)
+    # input = [[[0] * data.vocab_size for j in range(len(X[i]))] for i in range(len(X))]
 
     with tf.Graph().as_default(), tf.Session() as sess:
-        m = Model(training=True)
+        with tf.variable_scope('model', reuse=None):
+            m = Model(training=True, batch_size=batch_size)
+
+        with tf.variable_scope('model', reuse=True):
+            mvalid = Model(training=False, batch_size=len(valid_data))
+            mtest = Model(training=False, batch_size=len(test_data))
+
         sess.run(tf.initialize_all_variables())
-        state = sess.run(m.initial_state)
+        saver = tf.train.Saver
+        latest_chk = tf.train.latest_checkpoint('chk')
+
+        if latest_chk:
+            saver.restore(sess, latest_chk)
+        else:
+            saver.save(sess, 'chk/model-init')
+
+        for i in range(max_epoch):
+            # print("Epoch: %d Learning rate: %.3f" % (i + 1, sess.run(m.lr)))
+            train_perplexity = run_epoch(sess, m, train_data, m.train_op,
+                                         verbose=True)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+            valid_perplexity = run_epoch(sess, mvalid, valid_data, tf.no_op())
+            print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+            saver.save(sess, 'chk/model', global_step=i)
+
+        test_perplexity = run_epoch(sess, mtest, test_data, tf.no_op())
+        print("Test Perplexity: %.3f" % test_perplexity)
+
+        """state = sess.run(m.initial_state)
         cost, state, _ = sess.run([m.cost, m.final_state, m.train_op],
                                   {
                                       m.input: X,
@@ -36,7 +75,7 @@ def main():
                                       m.initial_state: state
                                   })
 
-    print(cost)
+    print(cost)"""
 
 
 if __name__ == '__main__':
